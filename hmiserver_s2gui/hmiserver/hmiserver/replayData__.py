97d380e3541f502db547a072e21dc981981ba210
ssrselvamraju <ssr30@cmu.edu>
2013-11-07 18:22:07 -0500

    Added replay debugging

--- hmiserver_s2gui/hmiserver/hmiserver/replayData__.py
+++ hmiserver_s2gui/hmiserver/hmiserver/replayData__.py
@@ -67,6 +67,26 @@ from mbprotocols import ModbusExtData
 
 ############################################################
 
+#Function Defenitions
+
+def buildData(full_data):
+	for ln in full_data:
+		if ln[4] > 0:
+			break
+	built_data = [[full_data[full_data.index(ln)-1][4], full_data[full_data.index(ln)-1][5]]]
+	for ln in full_data[full_data.index(ln):]:
+		built_data.append([abs(ln[4]), ln[5]])
+	return built_data
+
+def read_encoder(a):
+	return a+10
+
+def read_encoder1(a):
+	return a
+############################################################
+
+
+
 
 # Initialise the client. First get the remote host parameters.
 #hbhost, hbport, hbtimeout, hbunitid = CmdOpts.GetRemoteParams()
@@ -77,9 +97,9 @@ from mbprotocols import ModbusExtData
 #HBClient = ModbusClient.DataTableAccess(hbhost, hbport, hbtimeout, hbunitid)
 
 #HBClient1 = ModbusClient.DataTableAccess('192.168.10.237', 1502, 5.0, 1)
-replayDataClient1 = ModbusClient.DataTableAccess('10.0.0.100', 1502, 5.0, 1)
+#replayDataClient1 = ModbusClient.DataTableAccess('10.0.0.100', 1502, 5.0, 1)
 
-ExtData4 = ModbusExtData.ExtendedDataTypes(replayDataClient1)
+#ExtData4 = ModbusExtData.ExtendedDataTypes(replayDataClient1)
 
 ############################################################
 
@@ -91,46 +111,79 @@ ExtData4 = ModbusExtData.ExtendedDataTypes(replayDataClient1)
 file = open("truckData.dat","r")
 file.readline()
 file.readline()
+truckspeed = 700
+
 
 data = [map(float, line.split()) for line in file]
 data_end = len(data)-1
 
-truckspeed = 700
+data_to_follow = buildData(data)
 
-i = 0
+#print data_to_follow
+
+truck_angle = 0.24
+truck_const_speed = 700
+index = 1
+a=0
+print "Here"
+print data_to_follow[len(data_to_follow)-3][0]
+while read_encoder1(a) < data_to_follow[len(data_to_follow)-3][0]:
+	print "getting here"
+	print str(a) + " " + str(data_to_follow[index][0]) +" " + str(data_to_follow[index-1][0])
+	while read_encoder(a) < data_to_follow[index][0] and read_encoder1(a) > data_to_follow[index-1][0]:
+		print truck_angle
+		print a
+		truck_angle = data_to_follow[index-1][1]
+		
+	index = index+1
+
+truck_speed = 0
+
+
+
+
+#i = 0
 
 #Can try with file open here
 
-while i < data_end
+#while i < data_end
 
-	i=i+1;
+#	i=i+1;
 
 
 ####ALGO
 
-#read truck speed
+#read all encoder values and clean them up and store in another register ######start with making only forward motion work, if reverse motion throw error during this step
+#set angle at data[1]
+#give cont speed to truck
+#read truck encoders
+#while truck encoder near last 5-10 values > slow down (or brake for now)
+#while truck encoder is > data[i] and  < data[i+1] truck encoder = enc@data[i]
+#when this while breaks, i++ and back to while //Considering linear increments
 
 
 
 
 
-while True:
 
-	i = i+1
-	valVF = ExtData3.GetInpRegFloat32(20)
-	valVL = ExtData3.GetInpRegFloat32(22)
-	truckHeading = recDataClient1.GetInputRegistersInt(18)
-	tractionEncoder = recDataClient1.GetInputRegistersInt(12)
-	tillerAngle = ExtData3.GetInpRegFloat32(44)
 
-	print tractionEncoder
+#while True:
 
-	file.write( str(i) + '\t' + str(valVF) + '\t' + str(valVL) + '\t' + str(truckHeading) + '\t' + str(tractionEncoder) + '\t' + str(tillerAngle) + '\n' )
-	time.sleep(0.5)
+#	i = i+1
+#	valVF = ExtData3.GetInpRegFloat32(20)
+#	valVL = ExtData3.GetInpRegFloat32(22)
+#	truckHeading = recDataClient1.GetInputRegistersInt(18)
+#	tractionEncoder = recDataClient1.GetInputRegistersInt(12)
+#	tillerAngle = ExtData3.GetInpRegFloat32(44)
 
-	file.close()		
+#	print tractionEncoder
 
-	file = open("truckData.dat","a")			
+#	file.write( str(i) + '\t' + str(valVF) + '\t' + str(valVL) + '\t' + str(truckHeading) + '\t' + str(tractionEncoder) + '\t' + str(tillerAngle) + '\n' )
+#	time.sleep(0.5)
+
+#	file.close()		
+
+#	file = open("truckData.dat","a")			
 
 #		print "Here"
 #print "Here2"
